4a969aae9cf7d0d0370b4e528f9c873b56d7eee3
aseungbo <seungbo1112@gmail.com>
2021-11-16 14:53:54 +0900

    Finished CPP00 & Add CPP01 files

---
+++ code_cpp/cpp01/Zombie.hpp
@@ -0,0 +1,16 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+
+class Zombie
+{
+    private:
+        std::string name;
+
+    public:
+        Zombie(void);
+        ~Zombie(void);
+};
+
+#endif
