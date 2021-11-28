0c2de6241632a0d533c486262146e388d7d3a66e
seungb5 <seungbo1112@gmail.com>
2021-11-28 18:26:03 +0900

    Add virtual inheritance

--- code_cpp/cpp03/ex03/DiamondTrap.cpp
+++ code_cpp/cpp03/ex03/DiamondTrap.cpp
@@ -3,6 +3,9 @@
 DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name +  "_clap_name")
 {
     _Name = name;
+    set_Hp(FragTrap::get_Hp()); // 100
+    set_Ep(ScavTrap::get_Ep()); // 50
+    set_Ad(FragTrap::get_Ad()); // 30
     std::cout << "DiamondTrap constructor <" << _Name << "> called" << std::endl;
 }
 
