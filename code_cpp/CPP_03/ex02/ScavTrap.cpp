713f15f6108641ceb9f142ebf5faff4af6192198
seungb5 <seungbo1112@gmail.com>
2022-02-08 23:49:35 +0900

    Modified CPP03 Module

---
+++ code_cpp/CPP_03/ex02/ScavTrap.cpp
@@ -0,0 +1,81 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) 
+{
+    _Hp = 100;
+    _Ep = 50;
+    _Ad = 20;
+    _guard_mode = 0;
+    std::cout << "ScavTrap constructor <" << _Name << "> called" << std::endl;
+}
+
+ScavTrap::ScavTrap(void) : ClapTrap()
+{
+    _Hp = 100;
+    _Ep = 50;
+    _Ad = 20;
+    _guard_mode = 0;
+    std::cout << "ScavTrap constructor <" << _Name << "> called" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& st) : ClapTrap(st)
+{
+    *this = st;
+    std::cout << "ScavTrap Copy constructor <" << _Name << "> called" << std::endl;
+}
+
+ScavTrap::~ScavTrap(void)
+{
+    std::cout << "ScavTrap destructor <" << _Name << "> called" << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+    if (_Ep >= 25)
+    {
+        _guard_mode = 1;
+        _Ep -= 25;
+        std::cout << "ScavTrap " << _Name << " have enterred in Gate keeper mode." << std::endl;
+    }
+    else
+        std::cout << "ScavTrap " << _Name << " have not enough Energy." << std::endl;
+}
+
+void ScavTrap::attack(std::string const & target)
+{
+    if (_guard_mode == 1)
+        _guard_mode = 0;
+    std::cout << "ScavTrap " << _Name << " attacks " << target <<
+    " causing " << _Ad << " points of damage!" << std::endl;
+}
+
+void ScavTrap::takeDamage(unsigned int amount)
+{
+    if (_guard_mode == 1)
+    {
+        std::cout << "ScavTrap " << _Name << " defended the attack." << std::endl;
+        _guard_mode = 0;
+    }
+    else
+    {
+        _Hp -= amount;
+        std::cout << "ScavTrap " << _Name << " take " << amount << " damage. "
+        << "Now, ScavTrap " << _Name << " Hp: " << _Hp << std::endl;
+    }
+}
+
+void ScavTrap::beRepaired(unsigned int amount)
+{
+    _Hp += amount;;
+    std::cout << "ScavTrap " << _Name << " repaired " << amount << " Hp. "
+    << "Now, ScavTrap " << _Name << " Hp: " << _Hp << std::endl;
+}
+
+ScavTrap& ScavTrap::operator = (const ScavTrap& st)
+{
+    if (this == &st)
+	    return (*this);
+    ClapTrap::operator=(st);
+    _guard_mode = st._guard_mode;
+    return (*this);
+}
---
+++ code_cpp/CPP_03/ex02/ScavTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+    public:
+        ScavTrap(std::string name);
+        ScavTrap(void);
+        ScavTrap(const ScavTrap& st);
+        ~ScavTrap();
+        ScavTrap& operator = (const ScavTrap& st);
+        void guardGate();
+        void attack(std::string const & target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+
+    private:
+        int _guard_mode;
+};
+
+#endif
